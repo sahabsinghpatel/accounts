914c4be74f686af3570b1fed3229b6ca40794630
Pahab Patel <sahabsinghpatel07@gmail.com>
2021-11-06 22:48:15 +0530

    removed my email & password from setting

--- accounts/settings.py
+++ accounts/settings.py
@@ -120,5 +120,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sahabspatel07@gmail.com'
-EMAIL_HOST_PASSWORD = 'Phoenix&Sahab@1'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'email_password'
